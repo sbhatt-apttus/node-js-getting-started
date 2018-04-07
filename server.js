555509c082adee05761adb9a0d24d2e55fa98bc4
Shubham Bhatt <sbhatt@apttus.com>
2018-04-07 22:04:42 +0530

    added my github name

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var cors 		   = require('cors');
 var vkbeautify        = require('vkbeautify');
+var parseString = require('xml2js').parseString;
 
 var port = process.env.PORT || 8090; // set our port
 
